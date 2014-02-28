aadea28ae8005aaa81dc3f081f3882fee74e7394
RenatoFloresLima <renatofloreslima@gmail.com>
2014-02-28 15:09:19 -0300

    Ornização do codigo.
    Signed-off-by: RenatoFloresLima <renatofloreslima@gmail.com>

--- HelloWorld/src/Teste.java
+++ HelloWorld/src/Teste.java
@@ -1,9 +1,11 @@
 
 public class Teste {
- public static void main(String[] args) {
-	System.out.println("Teste");
-	System.out.println("Teste 55");
-	System.out.println("Funcionando?");
 	
-}
+	public static void main(String[] args) {
+		
+		System.out.println("Teste");
+		System.out.println("Teste 55");
+		System.out.println("Funcionando?");
+
+	}
 }
