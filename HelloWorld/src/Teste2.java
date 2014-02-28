4f82189ffb3a4e9ecb1536aef073840759e0aaf1
RenatoFloresLima <renatofloreslima@gmail.com>
2014-02-28 10:47:49 -0300

    Comitando sem comflito
    Signed-off-by: RenatoFloresLima <renatofloreslima@gmail.com>

--- HelloWorld/src/Teste2.java
+++ HelloWorld/src/Teste2.java
@@ -3,7 +3,7 @@ public class Teste2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Testando o merge om conflito");
+		System.out.println("Testando o merge sem conflito");
 
 	}
 
