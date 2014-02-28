4d4fe61c53b56d02fc71bf66385c48af83b5646e
RenatoFloresLima <renatofloreslima@gmail.com>
2014-02-28 10:53:14 -0300

    Comitando metodo imprimir

--- HelloWorld/src/Teste2.java
+++ HelloWorld/src/Teste2.java
@@ -1,10 +1,14 @@
 
 public class Teste2 {
 
+	public static void imprimir(String texto){
+		System.out.println(texto);
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Testando o merge sem conflito");
-
+		String texto = "Testo de teste.";
+		imprimir(texto);
 	}
 
 }
